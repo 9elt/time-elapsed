254b418ec8c54fd9dbe3fc4adc3ef26d184c7d79
9elt <nelt.primary@gmail.com>
2022-12-19 22:05:10 +0100

    docs

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A Rust crate that provides a concise and handy way to benchmark **elapsed time inside functions**.
 > time-elapsed brings a small overhead, however, if you are trying to measure very small durations (in the order of *nanoseconds* or few *microseconds*), please consider using something else.
 
-## installation
+### installation
 Add the following to Cargo.toml
 ```
 [dependencies]
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
 //! A Rust crate that provides a concise and handy way to benchmark **elapsed time inside functions**.
+//! > time-elapsed brings a small overhead, however, if you are trying to measure very small durations (in the order of *nanoseconds* or few *microseconds*), please consider using something else.
+//!
+//! ### installation
+//! Add the following to Cargo.toml
+//! ```
+//! [dependencies]
+//! time-elapsed = "0.1.0"
+//! ```
 //! 
 //! # features
 //! * named benchmark
